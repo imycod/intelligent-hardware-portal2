2888a2512de69c9e61458ff1960af5489484a9bc
wuxs <317009160@qq.com>
2025-03-27 12:54:14 +0800

    feat:h5

--- src/components/assemble.vue
+++ src/components/assemble.vue
@@ -1,31 +1,32 @@
 <template>
-  <footer class="footer-container border-t border-white bg-black text-white w-full">
+  <footer class="footer-container border-t border-white bg-black text-white w-full px-4">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      <div class="item text-center">
-        <h2>Contact Us</h2>
-        <a href="mailto:support@acumenbot.ai" class="block hover:text-gray-300 transition">support@acumenbot.ai</a>
+      <div class="item md:text-center">
+        <h2 class="text-xl md:text-3xl">Contact Us</h2>
+        <a href="mailto:support@acumenbot.ai"
+          class="block hover:text-gray-300 transition text-base md:text-2xl">support@acumenbot.ai</a>
       </div>
 
-      <div class="item text-center">
-        <h2>App Download</h2>
+      <div class="item md:text-center">
+        <h2 class="text-xl md:text-3xl">App Download</h2>
         <div class="colums">
-          <a href="#" class="block hover:text-gray-300 transition">Apple store</a>
-          <a href="#" class="block hover:text-gray-300 transition">Google store</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Apple store</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Google store</a>
         </div>
       </div>
 
-      <div class="item text-center">
-        <h2>More</h2>
+      <div class="item md:text-center">
+        <h2 class="text-xl md:text-3xl">More</h2>
         <div class="colums">
-          <a href="#" class="block hover:text-gray-300 transition">Privacy policy</a>
-          <a href="#" class="block hover:text-gray-300 transition">Refund policy</a>
-          <a href="#" class="block hover:text-gray-300 transition">Terms of use</a>
-          <a href="#" class="block hover:text-gray-300 transition">Support</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Privacy policy</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Refund policy</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Terms of use</a>
+          <a href="#" class="block hover:text-gray-300 transition text-base md:text-2xl">Support</a>
         </div>
       </div>
     </div>
 
-    <div class="copyright text-center">
+    <div class="copyright md:text-center text-lg md:text-xl mt-6 hidden md:block">
       <p>Copyright © 2025 Acumenbot LLC. All rights reserved.</p>
     </div>
   </footer>
@@ -36,16 +37,11 @@
   padding-top: 24px;
   padding-bottom: 10px;
 
-  .copyright {
-    margin-top: 24px;
-  }
-
   .grid {
     .item {
       h2 {
         color: #FFF;
         font-family: "SF Pro";
-        font-size: 32px;
         font-style: normal;
         font-weight: bold;
         line-height: normal;
@@ -61,21 +57,11 @@
       a {
         color: #FFF;
         font-family: "SF Pro";
-        font-size: 32px;
         font-style: normal;
         font-weight: lighter;
         line-height: normal;
       }
     }
-  }
-
-  .copyright {
-    color: #FFF;
-    font-family: "SF Pro";
-    font-size: 24px;
-    font-style: normal;
-    font-weight: lighter;
-    line-height: normal;
   }
 }
 </style>
