75b592c0760c50e55b7450e6fdf5a198813592a9
wuxs <317009160@qq.com>
2025-03-27 12:12:33 +0800

    feat:support style

--- src/views/support/index.vue
+++ src/views/support/index.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="mt-[228px] flex flex-col items-center w-full mb-[151px]">
-    <div class="w-[956px]">
-      <div class="q w-full text-center">HAVING ANY TROUBLE WITH ACUMENBOT?</div>
-      <div
-        class="form-container flex flex-col justify-center items-center gap-y-[30px]"
-      >
+  <div class="support-container">
+    <div class="form-container mx-auto">
+      <div class="title w-full">HAVING ANY TROUBLE WITH ACUMENBOT?</div>
+      <div class="form-wrapper">
         <div class="title">Contact us.</div>
         <form
           action=""
           class="w-full flex flex-col justify-center items-center"
         >
-          <input
-            type="text"
-            class="email mb-[30px]"
-            placeholder="Your Email address"
-          />
+          <input type="text" class="email" placeholder="Your Email address" />
           <textarea class="message mb-[30px]" placeholder="Message"></textarea>
           <button>Submit</button>
         </form>
@@ -24,64 +18,84 @@
 </template>
 
 <style lang="scss" scoped>
-.q {
-  color: #fff;
-  font-family: "SF Pro";
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 410;
-  line-height: normal;
-}
-.form-container {
-  .title {
-    color: #fff;
-    font-family: "SF Pro";
-    font-size: 80px;
-    font-style: normal;
-    font-weight: 410;
-    line-height: normal;
-  }
-  .email {
-    display: flex;
-    width: 100%;
-    padding: 24px 36px;
-    align-items: flex-start;
-    gap: 10px;
-    align-self: stretch;
-    border-radius: 50px;
-    border: 1px solid #fff;
-    background: rgba(255, 255, 255, 0.2);
-    color: #fff;
-    &::placeholder {
-      color: rgba(255, 255, 255, 0.5);
+.support-container {
+  margin-top: calc(var(--header-top) + var(--header-height) + 62px);
+  .form-container {
+    width: 956px;
+    .title {
+      color: #fff;
+      font-family: "SF Pro";
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 410;
+      line-height: normal;
     }
-  }
-  .message {
-    display: flex;
-    height: 500px;
-    width: 100%;
-    padding: 24px 36px;
-    align-self: stretch;
-    border-radius: 50px;
-    border: 1px solid #fff;
-    color: #fff;
-    background: rgba(255, 255, 255, 0.2);
-    &::placeholder {
-      color: rgba(255, 255, 255, 0.5);
+    .form-wrapper {
+      display: flex;
+      flex-direction: column;
+      gap: 30px;
+      .title {
+        color: #fff;
+        font-family: "SF Pro";
+        font-size: 80px;
+        font-style: normal;
+        font-weight: 410;
+        line-height: normal;
+      }
+      .email {
+        display: flex;
+        margin-bottom: 30px;
+        width: 100%;
+        padding: 24px 36px;
+        align-items: flex-start;
+        gap: 10px;
+        align-self: stretch;
+        border-radius: 50px;
+        border: 1px solid #fff;
+        font-family: "SF Pro";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 270;
+        line-height: normal;
+        background: rgba(255, 255, 255, 0.2);
+        color: #fff;
+        &::placeholder {
+          color: rgba(255, 255, 255, 0.5);
+        }
+      }
+      .message {
+        display: flex;
+        height: 500px;
+        width: 100%;
+        padding: 24px 36px;
+        align-self: stretch;
+        border-radius: 50px;
+        border: 1px solid #fff;
+        color: #fff;
+        font-family: "SF Pro";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 270;
+        line-height: normal;
+        background: rgba(255, 255, 255, 0.2);
+        &::placeholder {
+          color: rgba(255, 255, 255, 0.5);
+        }
+      }
+      button {
+        display: flex;
+        height: 90px;
+        padding: 10px 48px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        box-sizing: border-box;
+        border-radius: 50px;
+        background: #fbcbc1;
+        cursor: pointer;
+      }
     }
-  }
-  button {
-    display: flex;
-    height: 90px;
-    padding: 10px 48px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    box-sizing: border-box;
-    border-radius: 50px;
-    background: #fbcbc1;
-    cursor: pointer;
   }
 }
 </style>
